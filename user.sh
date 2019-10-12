c1dff6e488356ae9fec09798255cb84b82351ddb
ericsium <ericsium@users.noreply.github.com>
2019-10-11 23:41:09 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,3 +3,8 @@ git clone https://github.com/ericsium/dotfiles.git
 mv .bashrc .bashrc.old.$$
 (cd dotfiles && ./install)
 curl https://pyenv.run | bash
+source ~/.bashrc
+pyenv install 3.6.9
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
+
