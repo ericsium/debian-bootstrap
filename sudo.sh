91deb50e740ee2189fa87cc79e2da565ba0362a2
ericsium <ericsium@users.noreply.github.com>
2019-10-12 13:23:44 -0500

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+fd=0
+if [ -t "$fd" ]
+then
+  echo interactive
+else
+  echo non-interactive
+fi
+
 set -x
 # bootstrap with: wget -qO- <github_raw_url> | bash
 DEBIAN_FRONTEND=noninteractive
@@ -7,12 +15,10 @@ if [ $? -ne 0 ]; then
     echo "Insert guest additions cd first"
     exit 1
 fi
-apt update -y && apt upgrade
-apt install -y dkms linux-headers-$(uname -r) build-essential
-mount /media/cdrom
-sleep 5
-(cd /media/cdrom; sudo sh ./VBoxLinuxAdditions.run)
-apt install -y git emacs
+apt update && apt upgrade
+apt install dkms linux-headers-$(uname -r) build-essential
+sh ./VBoxLinuxAdditions.run
+apt install git emacs
 
 #Python dependencies
-apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+apt install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
