b9837a1548eb382151711b8e21c197584cf6b288
ericsium <ericsium@users.noreply.github.com>
2019-10-12 15:07:27 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,7 +3,12 @@ set -x
 git clone https://github.com/ericsium/dotfiles.git
 mv .bashrc .bashrc.old.$$
 (cd dotfiles && ./install)
-exec $SHELL
+
+#Bootstrap pyenv for installs
+export PATH="/home/$USER/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 pyenv install 3.6.9
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 pyenv virtualenv 3.6.9 global-3.6.9
