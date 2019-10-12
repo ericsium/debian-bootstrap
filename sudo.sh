83571374b8e1e908d02acbe146f82515e96de995
ericsium <ericsium@users.noreply.github.com>
2019-10-12 10:43:43 -0500

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/sh -x
 # bootstrap with: wget -qO- <github_raw_url> | sh
+apt update -y && apt upgrade
+apt install -y dkms linux-headers-$(uname -r) build-essential
 mount /media/cdrom
 if [ $? -ne 0 ]; then
     echo "Insert guest additions cd first"
     exit 1
 fi
-apt update -y && apt upgrade
-apt install -y dkms linux-headers-$(uname -r) build-essential
 sh /media/cdrom/VBoxLinuxAdditions.run
 apt install -y git emacs
 
