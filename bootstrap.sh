403b63b45f80f5aaa7e8f3c29fb7ba3a347e98e3
ericsium <ericsium@users.noreply.github.com>
2019-10-11 22:38:42 -0500

    Update guest additions install

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,6 @@ if [ $retval -ne 0 ]; then
     echo "Insert guest additions cd first"
     exit 1
 fi
-apt-get update && apt-get upgrade
-apt-get install build-essential module-assistant
-m-a prepare
+apt update -y && apt upgrade
+apt install dkms linux-headers-$(uname -r) build-essential
 sh /media/cdrom/VBoxLinuxAdditions.run
