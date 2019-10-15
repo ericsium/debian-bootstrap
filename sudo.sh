be42bda6d2197853f81d78163c71c2d28a2eb059
ericsium <ericsium@users.noreply.github.com>
2019-10-15 12:18:18 -0700

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -5,6 +5,9 @@
 
 set -x
 
+#######################
+# Virtualbox Guest Additions
+#######################
 mount /media/cdrom
 if [ $? -ne 0 ]; then
     echo "Insert guest additions cd first"
@@ -13,9 +16,15 @@ fi
 apt update -y && apt upgrade
 apt install -y dkms linux-headers-$(uname -r) build-essential
 sh /media/cdrom/VBoxLinuxAdditions.run
+
+#######################
+# Misc packages
+#######################
 apt install -y git emacs
 
-#Python dependencies
+#######################
+# Python Dependencies
+#######################
 apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 #######################
