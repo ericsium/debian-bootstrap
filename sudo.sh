ecd899456f8538c476eefcdcb15296279755c49c
ericsium <ericsium@users.noreply.github.com>
2019-10-15 12:16:07 -0700

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -17,3 +17,23 @@ apt install -y git emacs
 
 #Python dependencies
 apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+#######################
+# Docker CE Edition
+#######################
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
